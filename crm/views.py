5c1bfa6a5e2caf916a705a9e9c34c295f1df69e4
Alisher <dautovalisher33@gmail.com>
2024-07-16 22:12:41 +0500

    Create register view & render 'regiser.html' with UserCreationForm

--- crm/views.py
+++ crm/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def home_view(request):
     return render(request, 'crm/home.html')
+
+def register(request):
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'crm/register.html', context)
