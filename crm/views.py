3fa948b407b0b4127c49a69a5f38fc1d96db14a4
Alisher <dautovalisher33@gmail.com>
2024-07-17 22:15:59 +0500

    Create forms.py & Change to RegisterForm & Add email to fileds

--- crm/views.py
+++ crm/views.py
@@ -1,17 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
-
+from .forms import RegisterForm
 
 # Create your views here.
 def home_view(request):
     return render(request, 'crm/home.html')
 
 def register(request):
-    form = UserCreationForm()
+    form = RegisterForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
